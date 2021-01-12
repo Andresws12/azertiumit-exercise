dd91a68443086b0780dd80bdb2ab25e97fa80d6e
afhg <andres.hernandez@star-group.net>
2021-01-12 11:19:01 +0100

    Fix lint: remove unnused code and fix format

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -3,8 +3,3 @@ addMatchImageSnapshotCommand({
     customSnapshotsDir: '/tests/e2e/snapshots',
     failureThreshold: 0.01,
 });
-
-Cypress.Commands.add('checkUI', name => {
-    cy.wait(1500);
-    cy.matchImageSnapshot(name);
-});
